34fc463694ebea7414772d7f1852561eae06572a
Viktoryia Pashaliuk <vkkitel@gmail.com>
2020-06-12 14:48:58 +0300

    calculator

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+  <script>
+    'use strict';
+
 let price_edged = 260;
   let price_unedged = 100;
   
@@ -6,11 +13,15 @@ let price_edged = 260;
   
   if document.getElementsByClassName("classEdged") {
   let result_edged = [i] * let price_edged 
-  } else {
-  document.getElementsByClassName("classUnedged")
+  } if document.getElementsByClassName("classUnedged") {
   let result_unedged = [i] * let price_unedged
   document.getElementById("result_edged").innerHTML = result_edged
   document.getElementById("result_unedged").innerHTML = result_unedged
   }
   }
-  
+  </script>
+
+
+</body>
+
+</html>
