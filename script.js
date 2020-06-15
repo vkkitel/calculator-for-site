79cc038546bcb3f24821b9fd682bd17d8ae206ad
Viktoryia Pashaliuk <vkkitel@gmail.com>
2020-06-15 19:28:06 +0300

    add obj

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-
-
-let price_edged = 260;
-let price_unedged = 100;
   
   function arrayMulti() {
-  let numbers = []
+    let board = {
+         price_edged = 260;
+         price_unedged = 100;
+      };
+    
+    let numbers = [];
+
   for (let i = 0; i < numbers.length; i++) {
   
   if document.getElementsByClassName("classEdged") {
-  let result_edged = [i] * let price_edged 
-  } if document.getElementsByClassName("classUnedged") {
-  let result_unedged = [i] * let price_unedged
+  let result_edged = [i] * board.price_edged 
+  } else {
+  document.getElementsByClassName("classUnedged") {
+  let result_unedged = [i] * board.price_unedged
   document.getElementById("result_edged").innerHTML = result_edged
   document.getElementById("result_unedged").innerHTML = result_unedged
   }
